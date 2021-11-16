12aa62b89e2c5bd33f5d78e43b16bc086ae047a1
halim mulani <halimmln0@gmail.com>
2021-11-16 16:13:56 +0530

    active navigation

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,14 +3,15 @@ import './Navbar.css';
 import { Navbar, Nav, Form, Button } from 'react-bootstrap';
 
 const Navigation = () => {
-  
+  const pathname = window.location.pathname
+  console.log(pathname)
   return (
     <Navbar bg="primary" variant="dark">
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="/">UserList</Nav.Link>
+          <Nav.Link className = {pathname == '/' || pathname=='/userList' ? 'active':''} href="/">UserList</Nav.Link>
           {/* <Nav.Link href="/user">AddUser</Nav.Link> */}
-          <Nav.Link href="/userForm">User form</Nav.Link>
+          <Nav.Link className = { pathname=='/userForm' ? 'active':''}  href="/userForm" >User form</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
